607eae3211a1d4e6fc5b3136970ced5980758de7
Megan Mai <75729899+MeganMai@users.noreply.github.com>
2022-07-19 16:41:06 +0100

    Delete Q1. regional_sales_US.sql

--- Q1. regional_sales_US.sql
+++
@@ -1,6 +0,0 @@
-ALTER VIEW region_sales_US AS
-SELECT Name as region, SUM(SalesYTD) Sales_YTD, SUM(SalesLastYear) Sales_LastYear
-FROM sales.SalesTerritory
-WHERE CountryRegionCode IN (
-							SELECT country_code FROM top_performing_country)
-GROUP BY Name
